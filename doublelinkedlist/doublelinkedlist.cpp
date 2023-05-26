7840d721fca90cf786ee79bbf31dfc3dfa91d7fa
danardi07 <danar.adi.ft22@mail.umy.ac.id>
2023-05-26 20:54:17 +0700

    void DoubleLinkedList::addNode()

--- doublelinkedlist/doublelinkedlist.cpp
+++ doublelinkedlist/doublelinkedlist.cpp
@@ -29,7 +29,7 @@ DoubleLinkedList::DoubleLinkedList(){
     START = NULL;
 }
 
-void DoubleLinkedList::addNode() {
+void DoubleLinkedList::addNode() { 
 	int nim;
 	string nm;
 	cout << "\nEnter the roll number of the student: ";
